c30bbc00bd403ee453eb13b54d5af1b7037871de
Mohemed ansad mk <92428126+ansadmk@users.noreply.github.com>
2023-06-06 11:16:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/Fetching-the-objects-properties-and-displaying-using-react-and-fetch-wk_01_Day_04-/'
 })
